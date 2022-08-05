035fd0b9c5a3a90472d89e589a1ad7fd699f90eb
O-Robot <adewaleogooluwani@gmail.com>
2022-08-05 12:38:46 +0100

    newer update

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -6,7 +6,7 @@ import Button from "../../components/button/Button";
 import TopButton from "../../components/topButton/TopButton";
 import { Fade } from "react-reveal";
 import { projectsHeader } from "../../portfolio.js";
-import ProjectsData from "../../shared/opensource/projects.json";
+import ProjectsData from "../../shared/projects.json";
 import "./Projects.css";
 import ProjectsImg from "./ProjectsImg";
 
