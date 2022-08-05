0286a8e77d2c1550acd0113f0b771c9dbd8b279c
O-Robot <adewaleogooluwani@gmail.com>
2022-08-05 11:43:03 +0100

    new

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -15,28 +15,28 @@ export default class Main extends Component {
     if (settings.isSplash) {
       return (
         <div>
-          <HashRouter basename={process.env.PUBLIC_URL}>
+          <HashRouter basename="/">
             <Switch>
               <Route
-                path={`${process.env.PUBLIC_URL}/`}
+                path="/"
                 exact
                 render={(props) => (
                   <Splash {...props} theme={this.props.theme} />
                 )}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/home`}
+                path="/home"
                 render={(props) => <Home {...props} theme={this.props.theme} />}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/experience`}
+                path="/experience"
                 exact
                 render={(props) => (
                   <Experience {...props} theme={this.props.theme} />
                 )}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/education`}
+                path="/education"
                 render={(props) => (
                   <Education {...props} theme={this.props.theme} />
                 )}
@@ -48,19 +48,19 @@ export default class Main extends Component {
                 )}
               /> */}
               <Route
-                path={`${process.env.PUBLIC_URL}/contact`}
+                path="/contact"
                 render={(props) => (
                   <Contact {...props} theme={this.props.theme} />
                 )}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/splash`}
+                path="/splash"
                 render={(props) => (
                   <Splash {...props} theme={this.props.theme} />
                 )}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/projects`}
+                path="/projects"
                 render={(props) => (
                   <Projects {...props} theme={this.props.theme} />
                 )}
@@ -72,26 +72,26 @@ export default class Main extends Component {
     } else {
       return (
         <div>
-          <HashRouter basename={"/"}>
+          <HashRouter basename="/">
             <Switch>
               <Route
-                path={`${process.env.PUBLIC_URL}/`}
+                path="/"
                 exact
                 render={(props) => <Home {...props} theme={this.props.theme} />}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/home`}
+                path="/home"
                 render={(props) => <Home {...props} theme={this.props.theme} />}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/experience`}
+                path="/experience"
                 exact
                 render={(props) => (
                   <Experience {...props} theme={this.props.theme} />
                 )}
               />
               <Route
-                path={`${process.env.PUBLIC_URL}/education`}
+                path="/education"
                 render={(props) => (
                   <Education {...props} theme={this.props.theme} />
                 )}
@@ -103,7 +103,7 @@ export default class Main extends Component {
                 )}
               /> */}
               <Route
-                path={`${process.env.PUBLIC_URL}/contact`}
+                path="/contact"
                 render={(props) => (
                   <Contact {...props} theme={this.props.theme} />
                 )}
@@ -118,7 +118,7 @@ export default class Main extends Component {
 							)}
 						/> */}
               <Route
-                path={`${process.env.PUBLIC_URL}/projects`}
+                path="/projects"
                 render={(props) => (
                   <Projects {...props} theme={this.props.theme} />
                 )}
